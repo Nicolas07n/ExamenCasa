c5c539e7104170c7e2774c33e481f82be0b0b82b
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-15 18:15:15 +0200

    Player Movement
    Evaluacion 1 , Apartado 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,16 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 moveDirection = new Vector2(moveX, moveY);
+        rb.velocity = moveDirection * speed;
     }
 
     private void FixedUpdate()
