67343c43f004f32883db0a9e4a43be4855daa6f4
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-15 18:07:40 +0200

    Trabjo
    1

---
+++ Assets/Scripts/OOP/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType { AVERAGE_ENEMY, RANDOM_ENEMY, DUMB_ENEMY };
+
+public abstract class Enemy
+{
+    
+}
