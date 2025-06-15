2699f406df05c2720a9d94fb51436a2e93ee6fd4
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-15 19:54:33 +0200

    game manager
    todo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
+    public static GameManager instance;
+    private int cambiosDireccion = 0;
+    public int CambiosDireccion => cambiosDireccion;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+    
+        public void RegistrarCambioDireccion()
+        {
+         cambiosDireccion++;    
+
+        }
+     
+
+
+
+
 }
