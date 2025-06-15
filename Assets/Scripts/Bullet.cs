e9fb3474ad54d725e8a7c8c3f688bc53f6b7d5b9
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-15 19:00:16 +0200

    Bullet y shootBullet
    Primera Evaluacion, Apartado 2

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Vector2 dir;
-    public float speed;
-    private Rigidbody2D rb;
-
+    private Vector2 direction;
+    public float speed = 10f;
+    private Rigidbody2D _rb;
+    private int bounceCount = 0;
+    private int maxBounces = 2;
     // Start is called before the first frame update
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = direction.normalized * speed;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+     public void SetDirection(Vector2 dir)
+     {
+        direction = dir.normalized;
         
+
+     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        
+            return;
+            bounceCount++;  
+        
+   
+         if(bounceCount > maxBounces)
+         {
+            Destroy(gameObject);
+         }
+        else
+        {
+            direction = -direction;
+            _rb.velocity = direction * speed;   
+
+        }
+    
+    
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-    }
+
+
+
 }
--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ShootBullet : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate=0.3f;
+    private float nextFireTime;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,27 @@ public class ShootBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if (Input.GetMouseButton(0) && Time.time>= nextFireTime)
+       {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+       }
     }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position ,Quaternion.identity);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - firePoint.position; 
+        bullet.GetComponent<Bullet>().SetDirection(direction);
+
+
+
+    }
+
+
+
+
+
+
 }
